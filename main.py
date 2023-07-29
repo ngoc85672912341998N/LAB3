0b70f2455246ac7cc8d549aa08d77172e127d46b
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-29 08:48:51 +0700

    aaa

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
-
+import psycopg2
 app = FastAPI()
 
 @app.get("/")
 async def root():
+    onn = psycopg2.connect(database = "customer123",
+                        user = "ngoc",
+                        password = "CpNdfzvhEQCXVmL8RgssqscmohAKbzGe",
+                        host = "dpg-cj26rq98g3n1jki0gkj0-a",
+                        port = "5432")
     return {"message": "Hello from FastAPI!"}
 
 if __name__ == "__main__":
