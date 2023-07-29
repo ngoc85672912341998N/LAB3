26cad04a931c3383ad13c9d706481fa6f4cce4e7
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-29 09:28:47 +0700

    sss

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 import psycopg2
+from urlparse import urlparse
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    onn = psycopg2.connect(database = "customer123",
-                        user = "ngoc",
-                        password = "CpNdfzvhEQCXVmL8RgssqscmohAKbzGe",
-                        host = "dpg-cj26rq98g3n1jki0gkj0-a",
-                        port = "5432")
-    return {"message": "Hello from FastAPI!"}
+        result = urlparse("postgresql://postgres:postgres@localhost/postgres")
+	username = result.username
+	password = result.password
+	database = result.path[1:]
+	hostname = result.hostname
+	port = result.port
+	connection = psycopg2.connect(
+    		database = database,
+    		user = username,
+    		password = password,
+    		host = hostname,
+    		port = port
+		)
+    	return {"message": "Hello from FastAPI!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
