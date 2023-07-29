db8d34b5c1aab60167824a55463269e7fafd9230
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-29 08:20:36 +0700

    xxx

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -53,3 +53,6 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
