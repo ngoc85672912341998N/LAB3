571b266d6c1b5e3f544c0f0daec8568f41d8d98e
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-30 17:08:41 +0700

    cbvcb

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL ="sqlite:///./databa1se.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://default:lL4DKyesrYj0@ep-rough-sunset-20905830.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:vuYcO3HTQTx2xqNPV3Ow@containers-us-west-210.railway.app:6524/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
