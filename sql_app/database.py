1cd5b673b064fd4d77f2b09d0ddb60a862222a79
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-30 16:25:48 +0700

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL ="sqlite:///./databa1se.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://ngoc:CpNdfzvhEQCXVmL8RgssqscmohAKbzGe@dpg-cj26rq98g3n1jki0gkj0-a.oregon-postgres.render.com/customer123"
+SQLALCHEMY_DATABASE_URL = "postgres://ngoc:CpNdfzvhEQCXVmL8RgssqscmohAKbzGe@dpg-cj26rq98g3n1jki0gkj0-a/customer123"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
