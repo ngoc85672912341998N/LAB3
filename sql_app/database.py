3093bdd8954b928234790208784379cc240fae90
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-30 16:27:02 +0700

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # SQLALCHEMY_DATABASE_URL ="sqlite:///./databa1se.db"
 SQLALCHEMY_DATABASE_URL = "postgres://ngoc:CpNdfzvhEQCXVmL8RgssqscmohAKbzGe@dpg-cj26rq98g3n1jki0gkj0-a/customer123"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
