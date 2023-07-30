12e6d81769f08a3ec10cdee8df23be8a5547c88d
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-30 16:29:01 +0700

    Update main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -45,45 +45,6 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-@app.post("/create_mutiple_users/", response_model=schemas.User)
-def create_user(user: schemas.User, db: Session = Depends(get_db)):
-    return crud.create_mutiple_user(db=db, user=user)
-
-@app.post("/get_update_luot_thich/")
-def get_update_luot_thich(user: schemas.UpdatePost, db: Session = Depends(get_db)):
-    print(user)
-    return crud.get_update_user11(db=db, user=user)
-
-@app.post("/get_update_time_users/")
-def get_update1_user(user: schemas.UpdateTime, db: Session = Depends(get_db)):
-    print(user)
-    return crud.get_update_time_user11(db=db, user=user)
-
-
-@app.post("/get_Delete_time_users/")
-def get_Delete_time_users(user: schemas.DeleteTime, db: Session = Depends(get_db)):
-    print(user)
-    return crud.get_delete_time_user11(db=db, user=user)
-
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-##################################################################################
-
-@app.post("/create_update__time/", response_model=schemas.updateUser)
-def create_update__time(user: schemas.updateUser, db: Session = Depends(get_db)):
-    db_user = crud.get_update_user_by_email(db, thoi_gian=user.thoi_gian)
-    if db_user:
-        raise HTTPException(status_code=400, detail="time already registered")
-    return crud.create_update_user(db=db, user=user)
-
-
-@app.get("/read_update_time/", response_model=List[schemas.updateUser])
-def read_update_time(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_update_users(db, skip=skip, limit=limit)
-    return users
 
 
 
